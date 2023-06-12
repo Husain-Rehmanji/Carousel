713b94eca12be93e421040d82531910aeb94a2ba
Husain_R <husainrehmanji@gmail.com>
2023-06-12 17:43:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React from 'react';
 import Slider from './Components/Slider';
-
+import Footer from './Components/Footer';
 function App() {
-  return <Slider />;
-  
+  return (
+    <div>
+      <Slider />
+      <Footer />
+    </div>
+  );
 }
-
 export default App;

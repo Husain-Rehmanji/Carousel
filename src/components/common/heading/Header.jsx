c83eb838e7067d3d87fafbf0709531dbf381e776
Husain <husainrehmanji@gmail.com>
2023-06-19 18:02:13 +0530

    19 June

--- src/components/common/heading/Header.jsx
+++ src/components/common/heading/Header.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import Head from './Head';
 import './header.css';
 import { Link, useLocation } from 'react-router-dom';
 
@@ -13,66 +12,73 @@ const Header = () => {
 
   return (
     <>
-    <div className='fixed-header-container'>
-   
-      <header>
-        <nav className='flexSB'>
-          <ul className={click ? 'mobile-nav' : 'flexSB'} onClick={handleMenuClick}>
-            <li>
-              <Link to='/' className={location.pathname === '/' ? 'active' : ''}>
-                HOME
-              </Link>
-            </li>
-            <li>
-              <Link to='/about' className={location.pathname === '/about' ? 'active' : ''}>
-                ABOUT
-              </Link>
-            </li>
-            <li>
-              <Link to='/courses' className={location.pathname === '/courses' ? 'active' : ''}>
-                COURSES
-              </Link>
-            </li>
-            <li>
-              <Link to='/projects' className={location.pathname === '/projects' ? 'active' : ''}>
-                PROJECTS
-              </Link>
-            </li>
-            <li>
-              <Link to='/blog' className={location.pathname === '/blog' ? 'active' : ''}>
-                BLOG
-              </Link>
-            </li>
-            <li>
-              <Link to='/events' className={location.pathname === '/events' ? 'active' : ''}>
-                TEAM EVENTS
-              </Link>
-            </li>
-            <li>
-              <Link to='/faqs' className={location.pathname === '/faqs' ? 'active' : ''}>
-                FAQS
-              </Link>
-            </li>
-            <li>
-              <Link to='/review' className={location.pathname === '/review' ? 'active' : ''}>
-                REVIEWS
-              </Link>
-            </li>
-            <li>
-              <Link to='/contact' className={location.pathname === '/contact' ? 'active' : ''}>
-                CONTACT
-              </Link>
-            </li>
-          </ul>
+      <div className='fixed-header-container'>
+        <header>
+          <nav className='flexSB'>
+            <div className="nav-container">
+              <div className="logo">
+                <h1>OGGNtech</h1>
+              </div>
+            </div>
 
-          <div className='start'>
-            <div className='button'>GET A QUOTE</div>
-          </div>
-          <button className='toggle' onClick={() => setClick(!click)}>
-            {click ? <i className='fa fa-times'></i> : <i className='fa fa-bars'></i>}
-          </button>
-        </nav>
-      </header>
+            <ul className={click ? 'mobile-nav' : 'flexSB'} onClick={handleMenuClick}>
+              <li>
+                <Link to='/' className={location.pathname === '/' ? 'active' : ''}>
+                  HOME
+                </Link>
+              </li>
+              <li>
+                <Link to='/about' className={location.pathname === '/about' ? 'active' : ''}>
+                  ABOUT
+                </Link>
+              </li>
+              <li>
+                <Link to='/courses' className={location.pathname === '/courses' ? 'active' : ''}>
+                  COURSES
+                </Link>
+              </li>
+              <li>
+                <Link to='/projects' className={location.pathname === '/projects' ? 'active' : ''}>
+                  PROJECTS
+                </Link>
+              </li>
+              <li>
+                <Link to='/blog' className={location.pathname === '/blog' ? 'active' : ''}>
+                  BLOG
+                </Link>
+              </li>
+              <li>
+                <Link to='/events' className={location.pathname === '/events' ? 'active' : ''}>
+                  TEAM EVENTS
+                </Link>
+              </li>
+              <li>
+                <Link to='/faqs' className={location.pathname === '/faqs' ? 'active' : ''}>
+                  FAQS
+                </Link>
+              </li>
+              <li>
+                <Link to='/review' className={location.pathname === '/review' ? 'active' : ''}>
+                  REVIEWS
+                </Link>
+              </li>
+              <li>
+                <Link to='/contact' className={location.pathname === '/contact' ? 'active' : ''}>
+                  CONTACT
+                </Link>
+              </li>
+            </ul>
+
+            <div className='social'>
+              <div>
+                <div className="star">
+                  <Link to="/login" style={{ color: '#FFF' }}> LOG IN </Link>
+                </div>
+                <div className="star">SIGN UP</div>
+              </div>
+            </div>
+          </nav>
+        </header>
       </div>
     </>
   );
